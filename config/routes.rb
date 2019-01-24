bb0ca12bc35a8a6d26f79abbecde28e6483eeedf
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-24 00:59:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: %i[index  new create]
-  get 'article/:id', to: 'articles#show', as: :article
+  resources :articles, only: %i[index show new create]
   get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
   patch 'articles/:id', to: 'articles#update'
 end
