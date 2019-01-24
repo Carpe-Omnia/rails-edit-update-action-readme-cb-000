0e7a30cb549ab5d2ed06c0f375d2062f8002937e
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-24 01:02:34 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(title: params[:title], description: params[:description])
-    redirect_to articles_path(@article)
+    redirect_to '/articles/#{@article.id}'
   end
 
 end
